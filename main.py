855ad05079a771ed516dd08b1bbf8e2838a93ca1
pfampuerogonzalez-cloud <pfampuerogonzalez@gmail.com>
2026-03-11 20:43:40 +0000

    realizacion de algunos ejercicios M3-L2 python

---
+++ main.py
@@ -0,0 +1,22 @@
+a = [5,1,4,9,0]
+
+""" print(a[2]) """
+
+
+""" b = range(3,10) + range(20, 23) """
+
+
+c = [[1,2], [3,4,5], [6,7]]
+""" print(c[1][2])
+ """
+""" print(len(c))
+print(len(c[0])) """
+""" print(c[-1]) """
+""" print(c[-1][+1])
+ """
+d = ["perro", "gato", "jirafa", "elefante"]
+""" print(d.index("jirafa")) """
+
+e = ["a", a, 2 * a]
+""" print(e[0]==e[1]) """
+""" print(len(e)) """
